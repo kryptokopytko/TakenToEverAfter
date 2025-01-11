ec7a43597cdc0a50ec356f5053accdcc45edd6ae
Kornelia <kornelia.makowska.2002@gmail.com>
2025-01-11 11:31:28 +0100

    Extend init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+# Create PostgreSQL user and database.
+
+export $(grep -v '^#' .env | xargs)
+envsubst < init.sql.template > init.sql
+psql -U postgres -f init.sql
+rm init.sql
+
+# Create DJANGO apps.
+
 APPS=("accounts" "expenses" "guests" "photos" "preferences" "questionnaire" "seating" "tasks")
 
 cd backend
