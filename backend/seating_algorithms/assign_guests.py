1495122ea7882e9ec3f86a809bd2e50cdda858d1
Kornelia <kornelia.makowska.2002@gmail.com>
2025-01-30 16:30:48 +0100

    Add ommited file

--- backend/seating_algorithms/assign_guests.py
+++ backend/seating_algorithms/assign_guests.py
@@ -3,21 +3,6 @@ from .greedy import greedily_assign_people_to_tables
 from .annealing import optimize_table_assignment
 
 def assign(people, groups, weights, tables_with_ids, fast_algorithm=False):
-
-    data_to_write = {
-        'people': people,
-        'groups': groups,
-        'weights': weights,
-        'tables_with_ids': tables_with_ids,
-        'fast_algorithm': fast_algorithm
-    }
-    
-    # Zapisanie danych do pliku data.txt
-    with open("data.txt", "w") as file:
-        for key, value in data_to_write.items():
-            file.write(f"{key}: {value}\n")
-            
-
     tables_seats = [table["seats"] for table in tables_with_ids]
     tables_ids = [table["id"] for table in tables_with_ids]
 
